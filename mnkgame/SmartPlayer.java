040b20166bd3be8b9f05dad5e4be0d2bb2024121
Ruggi54 <simoruggi54@gmail.com>
2022-07-08 16:23:43 +0200

    Alphabeta pruning non funzionante
    la cella 1,1 non è libera mannaggia

--- mnkgame/SmartPlayer.java
+++ mnkgame/SmartPlayer.java
@@ -41,29 +41,109 @@ public class SmartPlayer implements MNKPlayer {
      */
     public MNKCell selectCell(MNKCell[] FC, MNKCell[] MC) {
         long start = System.currentTimeMillis();
+        MNKCell c;
         if(MC.length > 0) {
-            MNKCell c = MC[MC.length-1]; // Recover the last move from MC
+            c = MC[MC.length-1]; // Recover the last move from MC
             B.markCell(c.i,c.j);         // Save the last move in the local MNKBoard
         }
+        c=iterativeDeepening(FC, MC, start);
+        if(c==null){
+            c = FC[rand.nextInt(FC.length)];
+			B.markCell(c.i,c.j);
+        }
+        return c;
+    }
+
+    public MNKCell iterativeDeepening(MNKCell[] FC, MNKCell[] MC, long startTime){
+        int d = 0, alpha = -1, beta = 1, eval = -2;
+        boolean found = false;
+        int depth = FC.length;
+        MNKCell c;
+        while((System.currentTimeMillis()-startTime)/1000.0 <= TIMEOUT*(80.0/100.0) && !found && d < depth){
+            //alphabeta pruning
+            c = FC[d];
+            B.markCell(c.i, c.j);
+            eval = alphabetaPruning(FC, MC, false, alpha, beta, depth);
+            if(eval==1) return c;
+            else{
+                B.unmarkCell();
+            }
+            d++;
+        }
+        return null;
 
     }
 
-//    public MNKCell iterativeDeepening(MNKCell[] FC, MNKCell[] MC, long startTime){
-//        int d = 0, alpha = -1, beta = 1, eval = -2;
-//        boolean found = false;
-//        int depth = FC.length;
-//        while((System.currentTimeMillis()-start)/1000.0 <= TIMEOUT*(99.0/100.0) && !found && d < depth){
-//            //alphabeta pruning
-//            eval = alphabetaPruning(FC, MC, alpha, beta, depth);
-//            d++;
-//        }
-//
-//    }
+    public int alphabetaPruning(MNKCell[] FC, MNKCell[] MC, boolean PlayerA, int alpha, int beta, int depth){
+        int eval=0;
+        if(FC.length == 1){
+            MNKCell c = FC[0];
+            if(B.markCell(c.i, c.j) == myWin){
+                B.unmarkCell();
+                return 1;
+            }
+            else if(B.markCell(c.i, c.j) == yourWin){
+                B.unmarkCell();
+                return -1;
+            }
+            else return 0;
+        }
 
-    public MNKCell alphabetaPruning(MNKCell[] FC, MNKCell[] MC, boolean first, int alpha, int beta, int depth){
+        if(FC.length > 0){
+            MNKCell c = FC[0];
+            if(B.markCell(c.i, c.j) == myWin){
+                B.unmarkCell();
+                return 1;
+            }
+            else if(B.markCell(c.i, c.j) == yourWin){
+                B.unmarkCell();
+                return -1;
+            }
+        }
+        
+        if(PlayerA){
+            eval=-2;
+            for (int i=0; i<FC.length; i++){
+                MNKCell c = FC[i];
+                B.markCell(c.i, c.j);
+                
+                eval=Max(alphabetaPruning(FC, MC, !PlayerA, alpha, beta, depth-1), eval);
+                alpha=Max(alpha, eval);
+                B.unmarkCell();
+                if(alpha>=beta) break; 
+                
+            }
+        }
+        else{
+            eval=2;
+            for (int i=0; i<FC.length; i++){
+                MNKCell c = FC[i];
+                B.markCell(c.i, c.j);
+                
+                eval=Min(alphabetaPruning(FC, MC, !PlayerA, alpha, beta, depth-1), eval);
+                beta=Min(beta, eval);
+                B.unmarkCell();
+                if(alpha>=beta) break; 
+            }
+        }
+        return eval;
+
+        
         
 
     }
+
+    private int Min(int n1, int n2) {
+        if(n1<n2) return n1;
+        else return n2;
+    }
+
+
+    private int Max(int n1, int n2) {
+        if(n1>n2) return n1;
+        else return n2;
+    }
+
 
     public String playerName() {
         return "SmartPlayer";
